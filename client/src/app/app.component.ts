07b35815da252ebf957aa16951875f519cea187e
SudhanvaAlpha <39915944+SudhanvaAlpha@users.noreply.github.com>
2020-04-07 20:54:31 +0530

    aorta

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css','css/bootstrap.min.css']
 })
 export class AppComponent {
   title = 'acopolypto';

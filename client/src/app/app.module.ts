9c3c2abbe5f89d4cdc47150be0f9afd5908507df
ajeya-bhat <ajeyabs@gmail.com>
2020-04-13 17:45:51 +0530

    Removed err

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserhomeComponent } from './userhome/userhome.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {UserService} from './user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ContactusComponent } from './contactus/contactus.component';
 
@@ -18,9 +13,6 @@ import { ContactusComponent } from './contactus/contactus.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    UserhomeComponent,
     ContactusComponent
   ],
   imports: [
@@ -31,7 +23,7 @@ import { ContactusComponent } from './contactus/contactus.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [UserService],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }

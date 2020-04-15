e87e01f63f135c7e98fc5b99ea1e14e0371180b4
ajeya-bhat <ajeyabs@gmail.com>
2020-04-15 19:18:44 +0530

    Small bugs to be fixed

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -13,13 +13,29 @@ import { interval } from 'rxjs';
 export class CartComponent implements OnInit {
   public tp:number=0;
   public stri:String="";
+  public un:String;
+  public em:String;
+  public u:String;
+  public e:String;
+
   constructor(public cartService:CartService) { }
   public selectedContact :Cart;
   ngOnInit(): void {
     this.getc();
   }
 
+  geti(){
+    this.u=document.getElementById("un").nodeValue;
+    this.e=document.getElementById("em").nodeValue;
+  }
+
   Buyer(){
+    this.geti();
+    console.log(this.stri);
+    console.log(this.tp);
+    console.log(this.un);
+    console.log(this.em);
+    
     if(confirm(" You can check out more games!!Buy now?")==true){
       this.cartService.dcart().subscribe((res)=>{
         this.getc();
@@ -32,10 +48,16 @@ export class CartComponent implements OnInit {
   getc(){
     this.cartService.getcart().subscribe((res)=>{
       this.cartService.carts=res as Cart[];
-      for(let i=0;i<this.cartService.carts.length;i++){
+      for(let i=0;i<this.cartService.carts.length-1;i++){
         this.tp+=this.cartService.carts[i].price;
-        this.stri
+        this.stri+=this.cartService.carts[i].name;
+        this.stri+=",";
     }
+    this.tp+=this.cartService.carts[this.cartService.carts.length-1].price;
+        this.stri+=this.cartService.carts[this.cartService.carts.length-1].name;
+        let u=this.tp.toFixed(2);
+        this.tp=parseFloat(u);
+        console.log(this.tp);
     });
   
   }
@@ -43,6 +65,7 @@ export class CartComponent implements OnInit {
     if(confirm("Delete?")==true){
     this.cartService.delcart(_id).subscribe((res)=>{
       this.tp=0;
+      this.stri="";
       this.getc();
     });}
     

06e31f3b9fef2a258e63d823e556f669aaa1a34d
SudhanvaAlpha <39915944+SudhanvaAlpha@users.noreply.github.com>
2020-04-16 15:41:00 +0530

    3030320

--- client/src/app/chart1/chart1.component.ts
+++ client/src/app/chart1/chart1.component.ts
@@ -25,10 +25,10 @@ export class Chart1Component implements OnInit {
                   label: '# of games',
                   data: this.yvalues,
                   backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)'
+                      'rgba(255, 99, 132, 0.5)',
+              'rgba(54, 162, 235, 0.5)',
+              'rgba(255, 206, 86, 0.5)',
+              'rgba(75, 192, 192, 0.5)'
                   ],
                   borderColor: [
                       'rgba(255, 99, 132, 1)',
